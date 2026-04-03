9e57f871e93ef38241ef419835ec8808596f401b
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-04-03 20:00:26 +0200

    added skills file

--- src/app/components/landing-page/landing-page.ts
+++ src/app/components/landing-page/landing-page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero/hero';
 import { About } from './about/about';
+import { Skills } from './skills/skills';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [Hero, About],
+  imports: [Hero, About, Skills],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss',
 })
