04619261cef7a2145b1996f0f40e30354757f9f3
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-04-07 19:57:42 +0200

    added skills array with detailed descriptions and icons

--- src/app/components/landing-page/skills/skills.ts
+++ src/app/components/landing-page/skills/skills.ts
@@ -6,4 +6,19 @@ import { Component } from '@angular/core';
   templateUrl: './skills.html',
   styleUrl: './skills.scss',
 })
-export class Skills {}
+export class Skills {
+
+  skills = [
+    { name: 'HTML', icon: 'html.png', active: true },
+    { name: 'CSS', icon: 'css.png', active: true },
+    { name: 'JavaScript', icon: 'javascript.png', active: true },
+    { name: 'Material Design', icon: 'material_design.png', active: false},
+    { name: 'TypeScript', icon: 'typescript.png', active: true },
+    { name: 'Angular', icon: 'angular.png', active: true },
+    { name: 'Supabase', icon: 'supabase.png', active: false },
+    { name: 'Git', icon: 'git.png', active: true },
+    { name: 'Rest-API', icon: 'rest-api.png', active: true },
+    { name: 'Scrum', icon: 'scrum.png', active: false },
+    { name: 'Growth mindset', icon: 'growth_mindset.png', active: true, tooltip: true }
+  ];
+}
