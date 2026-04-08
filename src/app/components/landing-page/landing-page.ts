ddd9a62566f99c3ad66957da676ec040a9ed01f7
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-04-08 11:45:06 +0200

    added Projects to imports

--- src/app/components/landing-page/landing-page.ts
+++ src/app/components/landing-page/landing-page.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Hero } from './hero/hero';
 import { About } from './about/about';
 import { Skills } from './skills/skills';
+import { Projects } from './projects/projects';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [Hero, About, Skills],
+  imports: [Hero, About, Skills, Projects ],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss',
 })
