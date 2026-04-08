805050bd9d5c2462455ebd51671c18e12b7d02a4
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-04-08 16:13:25 +0200

    add project interface and initialize project list in Projects component

--- src/app/components/landing-page/projects/projects.ts
+++ src/app/components/landing-page/projects/projects.ts
@@ -1,9 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+interface Project {
+  name: string;
+  technologies: string[];
+  image: string;
+  active: boolean;
+}
+
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [CommonModule],
+  standalone: true,
   templateUrl: './projects.html',
   styleUrl: './projects.scss',
 })
-export class Projects {}
+export class Projects {
+
+  projects: Project[] = [
+    {
+      name: 'Join',
+      technologies: ['Angular', 'TypeScript', 'HTML', 'CSS', 'Firebase'],
+      image: 'public/images/projects/join.png',
+      active: true
+    },
+    {
+      name: 'El Pollo Loco',
+      technologies: ['HTML', 'CSS', 'JavaScript'],
+      image: 'images/el_pollo_loco.png',
+      active: true
+
+    },
+    {
+      name: 'DA Bubble',
+      technologies: ['Angular', 'Firebase', 'TypeScript'],
+      image: 'images/dabubble.png',
+      active: true
+    }
+];
+
+get activeProjects() {
+  return this.projects.filter(p => p.active);
+}
+}
