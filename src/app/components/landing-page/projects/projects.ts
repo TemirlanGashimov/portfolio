47e02a5616500bbb67cb0b2cd1d8682d452018e2
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-04-09 12:58:18 +0200

    enhance Projects component with detailed project properties and modal functionality

--- src/app/components/landing-page/projects/projects.ts
+++ src/app/components/landing-page/projects/projects.ts
@@ -1,11 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef, ViewChild } from '@angular/core';
 
 interface Project {
   name: string;
   technologies: string[];
   image: string;
+  modalImage: string;
   active: boolean;
+  description: string;
+  github: string;
+  live: string;
+  number: string;
 }
 
 
@@ -18,29 +23,114 @@ interface Project {
 })
 export class Projects {
 
+  selectedProject: Project | null = null;
+
+  techIcons: {[key: string]: string} ={
+    'HTML': 'icons/html_dialog.png',
+    'CSS': 'icons/css_dialog.png',
+    'JavaScript': 'icons/javascript_dialog.png',
+    'TypeScript': 'icons/typescript_dialog.png',
+    'Angular': 'icons/angular_dialog.png',
+    'Firebase': 'icons/firebase_dialog.png'
+
+  }
+
+
   projects: Project[] = [
     {
       name: 'Join',
       technologies: ['Angular', 'TypeScript', 'HTML', 'CSS', 'Firebase'],
-      image: 'public/images/projects/join.png',
-      active: true
+      image: 'images/projects/join.png',
+      modalImage: 'images/projects/join_dialog.png',
+      active: true,
+      number: '01',
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
+      github: '',
+      live: ''
+      
     },
     {
       name: 'El Pollo Loco',
       technologies: ['HTML', 'CSS', 'JavaScript'],
-      image: 'images/el_pollo_loco.png',
-      active: true
+      image: 'images/projects/el_pollo_loco.png',
+      modalImage: 'images/projects/el_pollo_loco_dialog.png',
+      active: true,
+      number: '02',
+      description: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
+      github: '',
+      live: ''
 
     },
     {
-      name: 'DA Bubble',
+      name: 'DABubble',
       technologies: ['Angular', 'Firebase', 'TypeScript'],
-      image: 'images/dabubble.png',
-      active: true
+      image: 'images/projects/dabubble.png',
+      modalImage: 'images/projects/dabubble_dialog.png',
+      active: true,
+      number: '03',
+      description: 'This App is a Slack Clone App. It revolutionizes team communication and collaboration with its intuitive interface, real-time messaging, and robust channel organization.',
+      github: '',
+      live: ''
     }
 ];
 
 get activeProjects() {
   return this.projects.filter(p => p.active);
+}
+
+openProject(project: Project) {
+  this.selectedProject = project;
+
+  document.body.classList.add('no-scroll');
+}
+
+closeProject() {
+  this.selectedProject = null;
+
+  document.body.classList.remove('no-scroll');
+}
+
+nextProject() {
+  if (!this.selectedProject) return;
+
+  const index = this.projects.findIndex(p => p === this.selectedProject);
+  const nextIndex = (index +1) % this.projects.length;
+
+  this.selectedProject = this.projects[nextIndex];
+}
+
+@HostListener('document:keydown.escape')
+handleEscape() {
+  if (this.selectedProject) {
+    this.closeProject();
+  }
+}
+
+@ViewChild('modalRef') modalRef!: ElementRef;
+
+@HostListener('document:keydown', ['$event'])
+handleTab(event: KeyboardEvent) {
+  if (!this.selectedProject || event.key !== 'Tab') return;
+
+  const focusableElements = this.modalRef.nativeElement.querySelectorAll(
+    'a, button, [tabindex]:not([tabindex="-1"])'
+  );
+
+  if (!focusableElements.length) return;
+
+  const firstElement = focusableElements[0];
+  const lastElement = focusableElements[focusableElements.length -1];
+
+  const isShift = event.shiftKey;
+
+  if (!isShift && document.activeElement === lastElement) {
+    event.preventDefault();
+    firstElement.focus();
+  }
+
+  if( isShift && document.activeElement === firstElement) {
+    event.preventDefault();
+    lastElement.focus();
+  }
 }
 }
