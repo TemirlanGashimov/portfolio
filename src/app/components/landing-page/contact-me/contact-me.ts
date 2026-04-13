7a1439021baf7cccd1c62e1fa5227e78d7697b78
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-04-13 19:45:16 +0200

    Implement contact form functionality with validation and submission handling

--- src/app/components/landing-page/contact-me/contact-me.ts
+++ src/app/components/landing-page/contact-me/contact-me.ts
@@ -1,9 +1,62 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgIf} from '@angular/common';
+
 
 @Component({
   selector: 'app-contact-me',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, NgIf],
   templateUrl: './contact-me.html',
   styleUrl: './contact-me.scss',
 })
-export class ContactMe {}
+export class ContactMe {
+
+  name: string = '';
+  email: string = '';
+  message: string = '';
+  privacyAccepted: boolean = false;
+
+  submitted: boolean = false;
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (!this.isFormValid()) {
+      return;
+    }
+      console.log('Form valid ✅');
+
+      this.name = '';
+      this.email = '';
+      this.message = '';
+      this.privacyAccepted = false;
+      this.submitted = false;
+    }
+  
+
+  isFormValid(): boolean {
+    return (
+      this.name.trim() !== '' &&
+      this.email.trim() !== '' &&
+      this.message.trim() !== '' &&
+      this.privacyAccepted
+    );
+  }
+
+  isNameInvalid() {
+    return this.submitted && this.name.trim() === '';
+  }
+
+  isEmailInvalid() {
+    return this.submitted && this.email.trim() === '';
+  }
+
+  isMessageInvalid() {
+    return this.submitted && this.message.trim() === '';
+  }
+
+  isCheckboxInvalid() {
+    return this.submitted && !this.privacyAccepted;
+  }
+}
