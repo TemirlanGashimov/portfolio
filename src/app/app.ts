a48898560a3c6bdb5581cbedc43f1f6dc17f534b
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-04-13 19:44:47 +0200

    Add Hero and Footer components to app imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LandingPage } from './components/landing-page/landing-page';
+import { Hero } from "./components/landing-page/hero/hero";
+import { Footer } from './components/landing-page/footer/footer';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LandingPage],
+  imports: [RouterOutlet, LandingPage, Hero, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
