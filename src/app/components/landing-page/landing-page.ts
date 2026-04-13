950dea2ea20df7b7819c07eca211a6b605695e6d
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-04-13 19:45:52 +0200

    Update landing page imports to include ContactMe component

--- src/app/components/landing-page/landing-page.ts
+++ src/app/components/landing-page/landing-page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { Hero } from './hero/hero';
 import { About } from './about/about';
 import { Skills } from './skills/skills';
 import { Projects } from './projects/projects';
+import { ContactMe } from './contact-me/contact-me';
+
+
 
 @Component({
   selector: 'app-landing-page',
-  imports: [Hero, About, Skills, Projects ],
+  imports: [ About, Skills, Projects, ContactMe],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss',
 })
