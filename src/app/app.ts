2599e63ecac0fd59b3f44aec3aaff37e6517df2f
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-04-13 19:51:03 +0200

    Remove LandingPage import from app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LandingPage } from './components/landing-page/landing-page';
+
 import { Hero } from "./components/landing-page/hero/hero";
 import { Footer } from './components/landing-page/footer/footer';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LandingPage, Hero, Footer],
+  imports: [RouterOutlet, Hero, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
