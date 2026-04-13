e731b60539bf1143a3c429d074797422a63c5da1
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-04-13 19:44:37 +0200

    Define application routes for landing page and legal notice components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LandingPage } from './components/landing-page/landing-page';
+import { LegalNotice } from './components/legal-notice/legal-notice';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: LandingPage},
+    { path: 'legal-notice', component: LegalNotice}, 
+    
+];
