2109545f452bc04015adbdbe6031483c12f53b88
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-04-14 13:39:07 +0200

    impot Header and delete hero

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { Hero } from "./components/landing-page/hero/hero";
+
 import { Footer } from './components/landing-page/footer/footer';
+import { Header } from './components/landing-page/header/header';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Hero, Footer],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
