d62b2f4bd49b0ada4fee3c3d220cc9193f893c63
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-04-14 13:40:22 +0200

    added functionality to header component

--- src/app/components/landing-page/header/header.ts
+++ src/app/components/landing-page/header/header.ts
@@ -6,4 +6,23 @@ import { Component } from '@angular/core';
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+
+  active: string = '';
+  
+  setActive(section: string) {
+    this.active = section;
+  }
+
+  currentLanguage: 'EN' | 'DE' = 'EN';
+
+  setLanguage(lang: 'EN' | 'DE') {
+    this.currentLanguage = lang;
+  }
+
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+}
