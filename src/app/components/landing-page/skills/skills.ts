176dfe31a1d0202204c3d3946bffbedbc69a955c
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-04-23 14:42:08 +0200

    style: add RouterLink to skills component imports for improved navigation

--- src/app/components/landing-page/skills/skills.ts
+++ src/app/components/landing-page/skills/skills.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-skills',
-  imports: [TranslatePipe],
+  imports: [TranslatePipe, RouterLink],
   templateUrl: './skills.html',
   styleUrl: './skills.scss',
 })
