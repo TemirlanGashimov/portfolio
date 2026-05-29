345d7a0e37dae3b35644ed9dbf41ef72aca30a49
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-05-30 00:04:49 +0200

    feat: add Footer and LegalNotice components with responsive styling and unit tests

--- src/app/components/landing-page/footer/footer.spec.ts
+++ src/app/components/landing-page/footer/footer.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Footer } from './footer';
+import { TranslateModule } from '@ngx-translate/core';
+import { provideRouter } from '@angular/router';
 
 describe('Footer', () => {
   let component: Footer;
@@ -8,7 +9,8 @@ describe('Footer', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Footer],
+      imports: [Footer, TranslateModule.forRoot()],
+      providers: [provideRouter([])]
     }).compileComponents();
 
     fixture = TestBed.createComponent(Footer);
